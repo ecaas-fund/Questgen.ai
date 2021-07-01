08907be2d1ece215057aedc1e2a0016c4efe18dc
bipinkc19 <41479925+bipinkc19@users.noreply.github.com>
2021-07-01 22:33:51 +0545

    Update main.py

--- Questgen/main.py
+++ Questgen/main.py
@@ -2,6 +2,7 @@ import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import time
 import torch
+import random
 from transformers import T5ForConditionalGeneration,T5Tokenizer
 
 
